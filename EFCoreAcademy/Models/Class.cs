e725a23c9a07c0b94ad92a2c48eb0e2db5f454bb
LaylaRichard <lri@opinum.com>
2024-01-24 10:56:32 +0100

    Add models

---
+++ EFCoreAcademy/Models/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreAcademy;
+
+public class Class : BaseEntity
+{
+    public string Title { get; set; }
+    public Professor Professor { get; set; }
+    public List<Student> Students { get; set; }
+    
+}
---
+++ EFCoreAcademy/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreAcademy;
+
+public class Student:  BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public List<Class> Classes { get; set; }
+    public Address Address { get; set; }
+}
