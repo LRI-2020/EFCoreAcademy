105805aa1dd81473a209532eb7f2e466dbcf0d0e
LaylaRichard <lri@opinum.com>
2024-01-26 13:20:32 +0100

    demo efcore basics

--- EFCoreAcademy/Models/Class.cs
+++ EFCoreAcademy/Models/Class.cs
@@ -2,8 +2,14 @@
 
 public class Class : BaseEntity
 {
-    public string Title { get; set; }
-    public Professor Professor { get; set; }
-    public List<Student> Students { get; set; }
-    
+    public string Title { get; set; } = default!;
+    public Professor Professor { get; set; } = default!;
+    public List<Student> Students { get; set; } = default!;
+
 }
+
+// one to many //class >-----| professor  // - mandatory professor
+// many to many // class >------< students // - mandatory student
+// one to one //// professor |-----------| address (address is opt - professor mandatory)
+// one to one //// student |-----------| address (address is opt - student mandatory)
+// 
